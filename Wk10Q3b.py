cd79a4df07471e1502bfd29834a742ba41259990
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:37:01 +0800

    Update Wk10Q3b.py

--- Wk10Q3b.py
+++ Wk10Q3b.py
@@ -1,16 +1,16 @@
 def moduleswitching(mod):
-    match mod:
-        case "CSC1009":
+    match mod:                      #Switch case
+        case "CSC1009":             #mod = CSC1009
             return "Object-Oriented Programming"
-        case "CSC1006":
+        case "CSC1006":             #mod = CSC1006
             return "Mathematics 2"
-        case "CSC1007":
+        case "CSC1007":             #mod = CSC1007
             return "Operating Systems"
-        case "CSC1008":
+        case "CSC1008":             #mod = CSC1008
             return "Data Structures and Algorithms"
-        case "CSC1010":
+        case "CSC1010":             #mod = CSC1010
             return "Computer Networks"
-        case _:
+        case _:                     #mod is not one of the above cases
             return "Invalid"
 
 x = input("Module code:")
