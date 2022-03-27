06db55771a162cc1a90a35e94e8675cfb84207ec
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:35:19 +0800

    Update Wk10Q2.py

--- Wk10Q2.py
+++ Wk10Q2.py
@@ -1,4 +1,4 @@
-x = float(input("Enter 1st value: "))
+x = float(input("Enter 1st value: "))   #User input float values
 y = float(input("Enter 2nd value: "))
-avg = (x+y)/2
+avg = (x+y)/2   #Average of 1st and 2nd value
 print("The average of", x, "and",y, "=", avg)
